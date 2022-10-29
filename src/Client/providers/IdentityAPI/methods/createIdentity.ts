39c82916fc6d20c34b2d75a99271be7d79d0107a
Alex Werner <obusco@gmail.com>
2022-10-29 06:32:47 +0200

    fix(IdentityAPI): missing create identity

--- src/Client/providers/IdentityAPI/methods/createIdentity.ts
+++ src/Client/providers/IdentityAPI/methods/createIdentity.ts
@@ -1,3 +1,19 @@
-export default async function createIdentity(){
+import Axios from "axios";
 
+export default async function createIdentity(username: string, account: any){
+    console.log('Create identity with uname', username);
+    if(!username) throw new Error('Username required');
+    const address = account.getAddress();
+    const publicKey = account.getPublic();
+
+    console.log({address, publicKey})
+    //@ts-ignore
+    const {endpoint} = this;
+
+    const url = `${endpoint.url}/identity/${username}`;
+    console.log({url});
+    // const signature = '123abc';
+    // @ts-ignore
+
+    return Axios.post(url, { publicKey, address });
 };
